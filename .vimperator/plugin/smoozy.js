38b85c4e6e2d311d626e45018220a84e214972b8
Jeff Wu <wuthefwasthat@gmail.com>
2017-01-11 19:50:22 -0800

    improvements to vimperator

--- .vimperator/plugin/smoozy.js
+++ .vimperator/plugin/smoozy.js
@@ -1,51 +1,14 @@
-// -*- mode: Javascript; js-indent-level: 2; -*-
-//
 // smoozy.js
 //
-// LICENSE: {{{
+// LICENSE:
 //   Copyright (c) 2009 snaka<snaka.gml@gmail.com>
 //   Copyright (c) 2011 yuttie<yuta.taniguchi.y.t@gmail.com>
 //
 //     distributable under the terms of MIT license.
 //     http://opensource.org/licenses/mit-license.php
-// }}}
 //
-// INFO: {{{
-var INFO = xml`
-<plugin name="smoozy.js" version="0.12.0"
-        href="https://github.com/yuttie/smoozy.js"
-        summary="j,kキーでのスクロールをスムースに"
-        lang="en_US"
-        xmlns="http://vimperator.org/namespaces/liberator">
-  <author email="snaka.gml@gmail.com" homepage="http://vimperator.g.hatena.ne.jp/snaka72/">snaka</author>
-  <author email="yuta.taniguchi.y.t@gmail.com" homepage="https://github.com/yuttie">yuttie</author>
-  <project name="Vimperator" minVersion="3.6"/>
-  <license>MIT license</license>
-  <p>j,k key scrolling to be smoothly.</p>
-  <h3 tag="smoozy_global_variables">Global vriables</h3>
-  <p>You can configure following variable as you like.</p>
-  <dl>
-    <dt>smoozy_scroll_amount</dt><dd>Scrolling amount(unit:px). Default value is 400px.</dd>
-    <dt>smoozy_interval</dt><dd>Scrolling interval(unit:ms). Default value is 20ms.</dd>
-  </dl>
-  <h3 tag="smoozy_example">Example</h3>
-  <p>Set scroll amount is 300px and interval is 10ms.</p>
-  <code><ex><![CDATA[
-    let g:smoozy_scroll_amount="300"
-    let g:smoozy_scroll_interval="10"
-  ]]></ex></code>
-  <h3 tag="smoozy_API">API</h3>
-  <code>smoozy.smoothScrollBy(amount);</code>
-  <p>Example</p>
-  <code><ex><![CDATA[
-    :js liberator.plugins.smoozy.smoothScrollBy(600)
-    :js liberator.plugins.smoozy.smoothScrollBy(-600)
-  ]]></ex></code>
-</plugin>`;
-// }}}
 
-let self = liberator.plugins.smoozy = (function() {
-  // Mappings  {{{
+liberator.plugins.smoozy = (function() {
   mappings.addUserMap(
     [modes.NORMAL],
     ["j", "<Down>"],
@@ -90,17 +53,13 @@ let self = liberator.plugins.smoozy = (function() {
       count: true
     }
   );
-  // }}}
-  // PUBLIC {{{
   var PUBLICS = {
     smoothScrollBy: function(impulse) {
       win = Buffer.findScrollableWindow();
       applyImpulse(impulse, win);
     }
   }
-  // }}}
 
-  // PRIVATE {{{
   function getScrollImpulse()  { return window.eval(liberator.globalVariables.smoozy_scroll_impulse || '1000'); }
   function getScrollInterval() { return window.eval(liberator.globalVariables.smoozy_scroll_interval || '16.67'); }
   function getScrollFriction() { return window.eval(liberator.globalVariables.smoozy_scroll_friction || '5000'); }
@@ -154,7 +113,6 @@ let self = liberator.plugins.smoozy = (function() {
     }
   }
 
-  // }}}
   return PUBLICS;
 })();
 // vim: sw=2 ts=2 et si fdm=marker:
