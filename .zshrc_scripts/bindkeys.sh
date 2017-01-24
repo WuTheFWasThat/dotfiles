333a558f565a8424b6d21409ca5d63f0063782a0
Jeff Wu <wuthefwasthat@gmail.com>
2017-01-24 10:28:13 -0800

    better up and down

--- .zshrc_scripts/bindkeys.sh
+++ .zshrc_scripts/bindkeys.sh
@@ -1,5 +1,13 @@
+# https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+# bindkey "^[[A" up-line-or-beginning-search # Up
+# bindkey "^[[B" down-line-or-beginning-search # Down
+
 # for emacs mode:
-bindkey \^U backward-kill-line
+bindkey '^u' backward-kill-line
 
 # set vi mode
 # bindkey -v
@@ -13,9 +21,11 @@ bindkey '^h' backward-char
 bindkey '^l' forward-char
 bindkey '^k' up-history
 bindkey '^j' down-history
-# ^k and ^j might not work in tmux (used for switching panges)
-bindkey '^p' up-history
-bindkey '^n' down-history
+# ^k and ^j might not work in tmux (used for switching panes)
+# bindkey '^p' up-history
+# bindkey '^n' down-history
+bindkey '^p' up-line-or-beginning-search
+bindkey '^n' down-line-or-beginning-search
 
 bindkey '^v' edit-command-line
 # bindkey -M vicmd v edit-command-line
